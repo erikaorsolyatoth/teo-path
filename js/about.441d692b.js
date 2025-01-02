cfb0bd98f1c61631f9771854b381c640b9e3a9ae
erikaorsolyatoth <erikaorsolyatoth@users.noreply.github.com>
2025-01-02 09:09:36 +0000

    deploy: 07d51f961a7025beeec54b7b7d9b13bb3823cd04

---
+++ js/about.441d692b.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkfamilypathways"]=self["webpackChunkfamilypathways"]||[]).push([[594],{5304:function(a,t,n){n.r(t),n.d(t,{default:function(){return i}});var u=n(6768),r=n(4232);const s={class:"about"};function e(a,t){return(0,u.uX)(),(0,u.CE)("div",s,[(0,u.Lk)("h1",null,(0,r.v_)(a.$t("about")),1)])}var c=n(1241);const f={},l=(0,c.A)(f,[["render",e]]);var i=l}}]);
+//# sourceMappingURL=about.441d692b.js.map
