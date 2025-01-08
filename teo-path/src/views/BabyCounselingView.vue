c7e5ed20201917b17e389f4302cbf792a4bfcf96
erikaorsolyatoth <parentpathway@proton.me>
2025-01-08 12:49:09 +0100

    Localization resources and hybrid navigation

---
+++ teo-path/src/views/BabyCounselingView.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="section-padding">
+    <h1>{{ $t('babyCounselingView.servicesTitle') }}</h1>
+    <h4>{{ $t('babyCounselingView.servicesDescription') }}</h4>
+
+    <h5>{{ $t('babyCounselingView.service1Title') }}</h5>
+    <ul>
+      <li>{{ $t('babyCounselingView.service1Description1') }}</li>
+      <ul>
+        <li>{{ $t('babyCounselingView.service1Description11') }}</li>
+        <li>{{ $t('babyCounselingView.service1Description12') }}</li>
+      </ul>
+      <li>{{ $t('babyCounselingView.service1Description2') }}</li>
+      <ul>
+        <li>{{ $t('babyCounselingView.service1Description21') }}</li>
+        <li>{{ $t('babyCounselingView.service1Description22') }}</li>
+      </ul>
+      <li>{{ $t('babyCounselingView.service1Description3') }}</li>
+      <ul>
+        <li>{{ $t('babyCounselingView.service1Description31') }}</li>
+      </ul>
+    </ul>
+
+    <h5>{{ $t('babyCounselingView.service2Title') }}</h5>
+    <ul>
+      <li>{{ $t('babyCounselingView.service2Description1') }}</li>
+      <ul>
+        <li>{{ $t('babyCounselingView.service2Description11') }}</li>
+      </ul>
+      <li>{{ $t('babyCounselingView.service2Description2') }}</li>
+      <ul>
+        <li>{{ $t('babyCounselingView.service2Description21') }}</li>
+        <li>{{ $t('babyCounselingView.service2Description22') }}</li>
+      </ul>
+      <li>{{ $t('babyCounselingView.service2Description3') }}</li>
+      <ul>
+        <li>{{ $t('babyCounselingView.service2Description31') }}</li>
+        <li>{{ $t('babyCounselingView.service2Description32') }}</li>
+      </ul>
+    </ul>
+
+    <h5>{{ $t('babyCounselingView.service3Title') }}</h5>
+    <ul>
+      <li>{{ $t('babyCounselingView.service3Description1') }}</li>
+      <ul>
+        <li>{{ $t('babyCounselingView.service3Description11') }}</li>
+        <li>{{ $t('babyCounselingView.service3Description12') }}</li>
+      </ul>
+      <li>{{ $t('babyCounselingView.service3Description2') }}</li>
+      <ul>
+        <li>{{ $t('babyCounselingView.service3Description21') }}</li>
+        <li>{{ $t('babyCounselingView.service3Description22') }}</li>
+      </ul>
+      <li>{{ $t('babyCounselingView.service3Description3') }}</li>
+      <ul>
+        <li>{{ $t('babyCounselingView.service3Description31') }}</li>
+      </ul>
+    </ul>
+
+    <h5>{{ $t('babyCounselingView.service4Title') }}</h5>
+    <ul>
+      <li>{{ $t('babyCounselingView.service4Description1') }}</li>
+      <ul>
+        <li>{{ $t('babyCounselingView.service4Description11') }}</li>
+        <li>{{ $t('babyCounselingView.service4Description12') }}</li>
+      </ul>
+      <li>{{ $t('babyCounselingView.service4Description2') }}</li>
+      <ul>
+        <li>{{ $t('babyCounselingView.service4Description21') }}</li>
+        <li>{{ $t('babyCounselingView.service4Description22') }}</li>
+      </ul>
+    </ul>
+
+    <h3>{{ $t('babyCounselingView.whyImportantTitle') }}</h3>
+    <h4>{{ $t('babyCounselingView.servicesDescription') }}</h4>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
