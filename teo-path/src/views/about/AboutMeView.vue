6d16574daca8fc4e993fb4814c084909d457b421
erikaorsolyatoth <era@teopath.com>
2025-02-18 22:03:49 +0100

    Content added

--- teo-path/src/views/about/AboutMeView.vue
+++ teo-path/src/views/about/AboutMeView.vue
@@ -1,10 +1,42 @@
 <template>
   <div class="section-padding">
-    <h1>{{ $t('sectionAboutMe.aboutMeText') }}</h1>
-    <LogoImageBackground/>
+    <h1>{{ $t('sectionAboutMe.aboutMe') }}</h1>
+    <div class="d-none d-md-flex">
+      <div class="column">
+        <h5>{{ $t('sectionAboutMe.slogen') }}</h5>
+      </div>
+      <div class="column second">
+        <h5>{{ $t('sectionAboutMe.aboutMeText') }}</h5>
+      </div>
+    </div>
+
+    <div class="d-flex flex-column d-md-none">
+      <div class="text-just">
+        <h6>{{ $t('sectionAboutMe.slogen') }}</h6>
+      </div>
+      <div class="text-just">
+        <h6>{{ $t('sectionAboutMe.aboutMeText') }}</h6>
+      </div>
+    </div>
   </div>
+  <LogoImageBackground/>
 </template>
 
 <script setup lang="ts">
   import LogoImageBackground from "@/components/LogoImageBackground.vue";
 </script>
+
+<style scoped>
+.column {
+  max-width: 50%;
+  text-align: justify;
+}
+
+.second {
+  padding-left: 20px;
+}
+
+.text-just {
+  text-align: justify;
+}
+</style>/
--- teo-path/src/views/about/AffiliationsView.vue
+++ teo-path/src/views/about/AffiliationsView.vue
@@ -1,10 +1,50 @@
 <template>
   <div class="section-padding">
-    <h1>{{ $t('sectionIssues.issuesText') }}</h1>
-    <LogoImageBackground/>
+    <h1>{{ $t('sectionAffiliations.professionalAffiliations') }}</h1>
+    <div class="d-none d-md-flex">
+      <div class="full-width">
+        <ul>
+          <li><h5>{{ $t('sectionAffiliations.profAff1') }}</h5></li>
+          <li><h5>{{ $t('sectionAffiliations.profAff2') }}</h5></li>
+          <li><h5>{{ $t('sectionAffiliations.profAff3') }}</h5></li>
+          <li><h5>{{ $t('sectionAffiliations.profAff4') }}</h5></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="d-flex flex-column d-md-none">
+      <div class="text-just">
+        <ul>
+          <li><h5>{{ $t('sectionAffiliations.profAff1') }}</h5></li>
+          <li><h5>{{ $t('sectionAffiliations.profAff2') }}</h5></li>
+          <li><h5>{{ $t('sectionAffiliations.profAff3') }}</h5></li>
+          <li><h5>{{ $t('sectionAffiliations.profAff4') }}</h5></li>
+        </ul>
+      </div>
+    </div>
   </div>
+  <LogoImageBackground/>
 </template>
 
 <script setup lang="ts">
-  import LogoImageBackground from "@/components/LogoImageBackground.vue";
+import LogoImageBackground from "@/components/LogoImageBackground.vue";
 </script>
+
+<style scoped>
+.column {
+  max-width: 50%;
+  text-align: justify;
+}
+
+.second {
+  padding-left: 20px;
+}
+
+.text-just {
+  text-align: justify;
+}
+
+.full-width {
+  width: 100%;
+}
+</style>/
